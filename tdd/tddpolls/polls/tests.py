da56673e3814840361849e925c7b975790525b67
Dora Szendrei <szendrgigi@gmail.com>
2019-05-23 22:25:48 +0200

    Basic view returns minimal HTML

--- tdd/tddpolls/polls/tests.py
+++ tdd/tddpolls/polls/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from polls.views import main_page
 
 class MainPageTest(TestCase):
@@ -7,3 +9,11 @@ class MainPageTest(TestCase):
     def test_root_url_resolves_to_main_page(self):
         main = resolve('/')
         self.assertEqual(main.func, main_page)
+
+    def test_main_page_returns_correct_HTML(self):
+        request = HttpRequest()
+        response = main_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertInHTML('<title>TDD Polls</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- tdd/tddpolls/polls/views.py
+++ tdd/tddpolls/polls/views.py
@@ -1,4 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-def main_page():
-    pass
+def main_page(request):
+    return HttpResponse('<html><title>TDD Polls</title></html>')
